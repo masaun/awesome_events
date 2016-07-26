a070b1109577ee84d011de58349c1a0378ccbc1b
masaun <unomasanori@unomasanori-no-MacBook-Air.local>
2016-07-26 10:16:09 +0900

    first commit

---
+++ app/controllers/welcomes_controller.rb
@@ -0,0 +1,5 @@
+class WelcomesController < ApplicationController
+  def index
+    @events = Event.where('start_time > ?', Time.zone.now).order(:start_time)
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,36 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20160724055111) do
+
+  create_table "events", force: :cascade do |t|
+    t.integer  "owner_id",   limit: 4
+    t.string   "name",       limit: 255,   null: false
+    t.string   "place",      limit: 255,   null: false
+    t.datetime "start_time",               null: false
+    t.datetime "end_time",                 null: false
+    t.text     "content",    limit: 65535, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "provider",   limit: 255
+    t.string   "uid",        limit: 255
+    t.string   "nickname",   limit: 255
+    t.string   "image_url",  limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
